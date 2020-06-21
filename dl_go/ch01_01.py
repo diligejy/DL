cbbc322e50a3e8693fb3ffbc448308c9c40da23b
diligejy <sjy049@gmail.com>
2020-06-21 15:21:44 +0900

    딥러닝과 바둑 1.1

--- dl_go/ch01_01.py
+++ dl_go/ch01_01.py
@@ -6,4 +6,7 @@ class GenericLinearFunction:
     def evaluate(self, x):
         return self.a * x + self.b
 
+height_to_weight = GenericLinearFunction(a = 4.2, b = -137)
+height_of_new_person = 73
+estimated_weight = height_to_weight.evaluate(height_of_new_person)
 
---
+++ dl_go/ch01_02.py
@@ -0,0 +1,8 @@
+def suggest_tags(image_data):
+    """
+    그림에 대한 태그 추천
+
+    입력값 : image_data는 비트맵 포맷 그림파일
+
+    출력값 : 추천 태그의 순위 리스트
+    """
