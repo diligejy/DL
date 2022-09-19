3daa2727ef653253ebc516b5265e0aac0eed6315
diligejy <sjy049@gmail.com>
2022-09-20 00:24:25 +0900

    add: stable diffusion

---
+++ Arjancodes/Refactor_DS/main.py
@@ -0,0 +1,4 @@
+import numpy as np 
+import torch 
+from sklearn.metrics import accuracy_score
+from tqdm import tqdm 
---
+++ References/Readme.md
@@ -0,0 +1,5 @@
+1. GNN
+
+- [개쉬운 그래프 뉴럴 네트워크 Graph Neural Network](https://www.youtube.com/watch?v=9eMbvfRM9_8)
+
+---
